cf5baa429c08a1d6987639e25c201199d24844ea
alexandercerutti <cerutti.alexander@gmail.com>
2016-05-21 03:00:06 +0200

    v1.2: more controls

--- README.md
+++ README.md
@@ -4,11 +4,41 @@ A simple Telegram installer for linux.
 
 ####Description
 
-At the current version (0.9.33), Telegram provides only two executable files (Telegram itself and its updater) and no installer like many other linux softwares. So, I decided to create this installer in bash for Linux to download, extract and find Telegram (if you already downloaded it). To let it search the already downloaded version, just type the name of the _archive_ (.tar.xz) straight after the script name, like this:
+At the current version (0.9.49), Telegram provides only two executable files (Telegram itself and its updater) and no installer like many other linux softwares. So, I decided to create this installer in bash for Linux to download, extract and find Telegram (if you already downloaded it).
+This script offers five ways to install Telegram: auto-detection, manual-detection, file, folder, download.
 
-        ./installTelegram.sh telegram.tar.xz
+#####Auto-detection
+    Auto-detection will try automatically to retrieve Telegram (in order) from _script argument_, _telegram.tar.xz archive_, _extracted folder_ , _download from the web_. Auto-detection has **--auto** suffix.
 
-If file control fails (not passed as argument and/or not found), the script looks for _telegram_ and _Telegram_ folders in its path. Otherwise, it tries to download and extract Telegram archive.
+    The _script argument_ is an archive (.tar.xz) placed somewhere in your pc. To start with it, you will need to start the script like this:
+
+        ./installTelegram.sh --auto <path-to-archive>.tar.xz
+
+    The _telegram.tar.xz archive_ is an archive already downloaded by telegram by the website of via the script.
+    The _extracted folder_ is a folder extracted from telegram.tar.xz, called **telegram** or **Telegram**.
+    _download from the web_, will download telegram from the official website.
+
+#####Manual-detection
+    Manual-detection will lists you all the available ways to install Telegram (like auto-detection method). Manual-detection has **--manual** suffix.
+
+        ./installTelegram.sh --manual
+
+    The _script argument_ is an archive (tar.xz) placed somewhere in your pc. To start with it, you will need to start the script like this:
+
+        ./installTelegram.sh --manual <path-to-archive>.tar.xz
+
+    If an option is not available, it will be not displayed. Each option has a 'keyword' with which you can start each of them.
+    For the other ways, please see _auto-detection_ options.
+
+
+#####File
+    File is a shoutcut to **--auto** _script argument_. It has the suffix **-f**;
+
+
+#####Folder and download
+    If none of the previous args are passed to the script, the script itself will scan its folder searching telegram folder (_extracted folder_). Else it will download and process the archive from Telegram official website (tdesktop.com/linux);
+
+
 Once found the extracted folder, the script will move it to **/usr/local/bin/** or **/opt/** (if the first cannot be found).
 On Linux, software indexing is done with **.desktop** files. The script will create and move the file _telegram.desktop_ to **/usr/share/applications/**.
 
--- installTelegram.sh
+++ installTelegram.sh
@@ -1,65 +1,165 @@
 #!/bin/sh
-#telegram installer
-FILE="$1";
+#simple telegram installer
+FILE="$2";
+PAR="$1";
+bs=$(basename "$0");
 dir=/usr/local/bin;
-clear; #cleans the console
+
+clear;
 echo "\n******************************************************";
 echo "\n  Welcome into Simple Telegram Installer";
 echo "  This program can both download Telegram and read it\n  from an archive (tar.xz format).\n"
 #using /bin/echo to not use Dash's echo command. This lets me style the text
 /bin/echo -e "  To link a file, abort this actual session by answering\n  'no' to the following question and then start the program\n  again like this:\n\n\t$ \e[1m./installtelegram.sh yourfile.tar.xz.\e[0m";
 echo "\n******************************************************\n";
-echo -n "  Are you ready to begin the installation? [y/n] >> ";
+
+echo -n "  Are you ready to begin the installation? The next step\n  will depend by arguments. [y/n] >> ";
 read m;
+
+case $PAR in
+    --auto)
+        # Cerca sequenzialmente in maniera automatica
+        if [ ! -z "$FILE" ] && [ -f $FILE ]; then
+            echo -n "  STI will install telegram from the archive passed as argument.\n"
+            METHOD="args";
+        elif [ -f telegram.tar.xz ]; then
+            echo "\n  telegram.tar.xz has been found in the script folder.\n  STI will install from this souce."
+            FILE=telegram.tar.xz;
+            METHOD="file";
+        elif [ -d telegram ] || [ -d Telegram ]; then
+            echo "\n  telegram folder has been found in the script folder.\n  STI will install from this source."
+            METHOD="folder";
+        else
+            echo "\n  STI will provide to download telegram. Please, make sure to be connected to internet."
+            METHOD="dw"
+        fi
+        ;;
+    --manual)
+        echo -n "  You have chosen to check manual ways to install Telegram.\n  Write the name written in brackets to choose.\n  Here's the available ways:\n";
+        # Fa scegliere la sorgente
+        if [ ! -z "$FILE" ] && [ -f $FILE ]; then
+            echo "  - Pass an archive as parameter to the script. (args)"
+        fi
+        if [ -f telegram.tar.xz ]; then
+            echo "  - Install from archive in script directory. (file)"
+        fi
+        if [ -d telegram ] || [ -d Telegram ]; then
+            echo "  - Install from extracted folder in script directory. (folder)"
+        fi
+            echo "  - Download the last version from the web. (dw)"
+        echo -n ">> ";
+        read METHOD;
+        ;;
+    -f)
+        METHOD="args"
+        ;;
+    *)
+        # checks the folder and the download.
+        if [ -d telegram ] || [ -d Telegram ]; then
+            METHOD="folder";
+        else
+            METHOD="dw";
+        fi
+        ;;
+esac
 case $m in
     [yY]|[sS])
     # Downloading or seeking for the the file / folder
-    if [ -z "$FILE" -o ! -f $FILE ]; then
-        if [ ! -d telegram -a ! -d Telegram ]; then
+    case $METHOD in
+        args)
+            # somewhere in your pc
+            if [ ! -z "$FILE" ] && [ -f $FILE ]; then
+                tar xfJ $FILE;
+            else
+                echo -n "File not found or unreachable. Insert a path or file. The script will be restarted. Write \"abort\" to abort.";
+                read $FILE;
+                if [ $FILE == "abort" ]; then
+                    exit;
+                else
+                    ./$bs --auto
+                    exit;
+                fi;
+            fi
+        ;;
+        file)
+            # already downloaded
+            # control if telegram folder already existing and removing it.
+            if [ -d "$dir/telegram" ]; then
+                rm -rf $dir/telegram;
+            fi
+            if [ -f telegram.tar.xz ]; then
+                tar xfJ telegram.tar.xz;
+            else
+                echo "telegram.tar.xz archive not found or unreachable. The program will be aborted."
+                exit;
+            fi
+        ;;
+        folder)
+            if [ ! -d telegram ] && [ ! -d Telegram ]; then
+                echo "Telegram and telegram folder not found. The program will be aborted."
+                exit;
+            fi
+        ;;
+        *)
             echo "  I'm downloading Telegram... Download speed may vary depending by your connection speed.\n  DO NOT DELETE ANY NEW FILE ON DESKTOP. Press CTRL-Z to abort.\n";
             echo "\n  Please, stand behind the yellow line.";
             #using /bin/echo to not use Dash's echo command. This lets me style the text
             /bin/echo -e "\e[93m. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\e[0m";
             /bin/echo -e "\e[103m\t\t\t\t\t\t\t\t\t\t \e[0m \n";
             sudo wget --progress=bar http://tdesktop.com/linux -O telegram.tar.xz --no-check-certificate;
-        fi
+            tar xfJ telegram.tar.xz;
+        ;;
+    esac
+    #Renaming the folder to lower case.
+    if [ -d Telegram ]; then
+        sudo mv Telegram telegram;
     fi
-    if [ ! -d telegram -a ! -d Telegram ]; then
-        tar xvfJ telegram.tar.xz
-        if [ -d Telegram ]; then
-            sudo mv Telegram telegram;
-        fi
-    fi
+    #Moving the icon
     if [ -f "telegram128.png" ]; then
         sudo cp telegram128.png telegram/;
     else
         echo "telegram128.png is missing. You will not see any icon.";
     fi
+    #Valid folder control (for /usr/local/bin)
     if [ ! -d $dir ]; then
-        dir=/opt;
         echo "There's a problem with the folder: /usr/local/bin/ seems not to exists. Your application will be located in /opt/";
+        dir=/opt;
     fi
-    sudo mv -i telegram $dir/;
-
+    #removing older telegram folder in $dir.
+    if [ -d $dir/telegram ]; then
+        sudo rm -rf $dir/telegram;
+    fi
+    #coping the new folder and deleting it from the download place.
+    sudo cp -af telegram $dir/;
+    sudo rm -rf telegram;
     #creating index file
     if [ ! -f "/usr/share/applications/telegram.desktop" ]; then
-        echo "Creating and moving telegram.desktop file...";
+        echo "  Creating and moving telegram.desktop file...";
         printf "%s\n" "[Desktop Entry]" "Encoding=UTF-8" "Name=Telegram" "Exec=$dir/telegram/Telegram" "Icon=$dir/telegram/telegram128.png" "Type=Application" "Categories=Network" "Comment=Telegram" "[Desktop Action Gallery]" "Exec=$dir/telegram/Telegram" "Name=Telegram" >| telegram.desktop;
         mv telegram.desktop /usr/share/applications/;
     else
-        echo "telegram.desktop already existing! The file doesn't need to be replaced.";
+        echo "  telegram.desktop already existing! The file doesn't need to be replaced.";
     fi
 
-    echo -n "Want to delete following 3 files? telegram.tar.xz, telegram icon, telegram index file [y/n]";
-    read m;
-    if echo $m | grep "^[yYsS]"; then
-        rm -rf telegram.tar.xz;
-        rm telegram128.png;
+    #Waste deletion control
+    if [ -f telegram.tar.xz ]; then
+        echo -n "  Would you delete the downloaded file (telegram.tar.xz)? [y/n] ";
+        read m;
+        if echo $m | grep "^[yYsS]" >>/dev/null 2>/dev/null; then
+            rm -rf telegram.tar.xz;
+        fi
     fi
-
-    echo "Ending...\nThank you to have used Simple Telegram Installer.";
+    if [ -d Telegram ] || [ -d telegram ]; then
+        echo -n "  Would you delete the telegram folder? [y/n] ";
+        read m;
+        if echo $m | grep "^[yYsS]" >>/dev/null 2>/dev/null; then
+            rm -rf telegram;
+            rm -rf Telegram;
+        fi
+    fi
+    echo "  Ending...\n  Thank you to have used Simple Telegram Installer.\n\n\n";
     ;;
 *)
-    echo "Okay, see you!";
+    echo "  Okay, see you!";
     ;;
 esac
